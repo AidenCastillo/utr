e34b82717cbc52f421720087615bbdfaeb7fb4c0
Aiden Castillo <76675161+AidenCastillo@users.noreply.github.com>
2023-01-31 18:22:24 +0000

    test change

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,17 @@
-import utr from '../src/index.js'
-import { convertToScore } from '../src/index.js';
+import utr from "../src/index.js";
+import { convertToScore } from "../src/index.js";
 
-test('UTR class', () => {
-    expect(utr).toBeDefined();
+test("UTR class", () => {
+  expect(utr).toBeDefined();
+});
+test("UTR class function", () => {
+  expect(utr.login).toBeDefined();
+  expect(utr.searchPlayer).toBeDefined();
+  expect(utr.getPlayer).toBeDefined();
+  expect(utr.getResults).toBeDefined();
+    
 });
 
-test('convertToScore function', () => {
-    expect(convertToScore).toBeDefined();
+test("convertToScore function", () => {
+  expect(convertToScore).toBeDefined();
 });
